c07695b20476481b42092523234e9a9f2a9fc22e
Surya <suryakant.subudhi@outlook.com>
2024-06-22 13:49:40 +0530

    feat: footer for docs

--- theme.config.jsx
+++ theme.config.jsx
@@ -2,6 +2,17 @@ export default {
     logo: <span>Orion® Docs</span>,
     project: {
       link: 'https://github.com/exprays/orion'
+    },
+    footer: {
+      text: (
+        <span>
+          MIT {new Date().getFullYear()} ©{' '}
+          <a href="https://nextra.site" target="_blank">
+            Orion
+          </a>
+          .
+        </span>
+      )
     }
     // ... other theme options
   }
