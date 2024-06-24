782326182432cd3024a0aa6796e27facff1319ca
Surya <suryakant.subudhi@outlook.com>
2024-06-24 21:47:39 +0530

    chore: update command docs

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,3 +1,5 @@
+import { useRouter } from 'next/router';
+
 export default {
     logo: <span>Orion® Docs</span>,
     project: {
@@ -12,6 +14,22 @@ export default {
           </a>
           .
         </span>
+      )
+    },
+    useNextSeoProps() {
+      const { asPath } = useRouter()
+      if (asPath !== '/') {
+        return {
+          titleTemplate: '%s – ORION'
+        }
+      }
+    },
+    banner: {
+      key: '1.0-release',
+      text: (
+        <a href="https://orion.thestarsociety.tech/changelog" target="_blank">
+          🎉 Orion 1.0.0 nightly is released. Read more →
+        </a>
       )
     }
     // ... other theme options
